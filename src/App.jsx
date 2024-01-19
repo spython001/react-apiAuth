3e73e5709bce2542e7b9c03a42ea896ca943e711
spython <emmanuelegbo01@gmail.com>
2024-01-19 16:34:11 +0100

    added packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react'
+import getCrimeData from './api'
 import './App.css'
 
 function App() {
+
+  const [popular, setPopular] = useState([])
+
+  useEffect (() => {
+    const fetchResult = async () => {
+      const crimeResult = await getCrimeData();
+      console.log(crimeResult);
+    }
+    fetchResult();
+  }, [])
 
   return (
       <div>
--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ const options = {
 
 try {
 	const response = await fetch(url, options);
-	const result = await response.json();
+	const result = await response.text();
 	console.log(result);
 } catch (error) {
 	console.error(error);
